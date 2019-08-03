b871b9e2ccc72e8926f39d2bc5b291f7e35b7ff3
Brian Simonton <bsimonton78@gmail.com>
2019-08-03 15:03:54 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -30,5 +30,16 @@ function showRepositories() {
 }
 
 
+function getCommits(el) {
+  const name = el.dataset.repo;
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', showCommits);
+  req.open('GET', 'https://api.github.com/repos/octocat/' + name + '/commits');
+  req.send();
+}
+
+
+
+
 
 
